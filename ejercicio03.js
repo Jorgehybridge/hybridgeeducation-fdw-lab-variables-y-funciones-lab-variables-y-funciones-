280a4f6722f97a2995a3cc75436734ad2513a4c9
Jorge Salcedo <jorge2410045@hybridge.education>
2026-01-28 21:52:37 -0600

    Add 'sumar' function to sum two numbers
    Implement 'sumar' function to add two numbers and log the result.

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,7 +1,14 @@
 // Ejercicio 3: Suma de Dos Números
 // Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
 
-//Escribe tu función aquí
+function sumar(a, b) {
+    const resultado = a + b;
+    console.log(resultado);
+    return resultado;
+}
+
+sumar(5, 3);
+
 
 
 // Exporta la función para que pueda ser utilizada en las pruebas
