a90b4fa3553e0a1aebfe33bd7f910308da75aac9
Jorge Salcedo <jorge2410045@hybridge.education>
2026-01-28 22:05:46 -0600

    Update ejercicio09.js

--- ejercicio09.js
+++ ejercicio09.js
@@ -1,7 +1,12 @@
 // Ejercicio 9: Calcular el promedio de 5 números
 // Declara una función llamada "calcularPromedio" que acepte 5 parámetros "n1", "n2", "n3", "n4", "n5" y devuelva el promedio de los 5 números.
 
-//escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "calcularPromedio" y que debe recibir exactamente 5 parámetros
+function calcularPromedio(n1, n2, n3, n4, n5) {
+    return (n1 + n2 + n3 + n4 + n5) / 5;
+}
+
+console.log(calcularPromedio(10, 20, 30, 40, 50)); // 30
+console.log(calcularPromedio(5, 5, 5, 5, 5)); // 5
 
 
 // Exporta la función para que pueda ser utilizada en las pruebas
